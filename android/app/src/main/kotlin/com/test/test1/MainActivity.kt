2951eef4e9cf2aafcc6b9f276648361f4748144d
Hossam <hossamfathi050@outlook.com>
2024-07-07 23:37:07 +0300

    test App flutter

---
+++ android/app/src/main/kotlin/com/test/test1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.test.test1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+  class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_circle_left_outlined),
+
+          title:
+          const Text(
+              'HOSSAM FATHI',
+          style: TextStyle(fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,)
+            ,),
+          centerTitle: true,
+actions: [
+
+  Icon(Icons.mobile_friendly),
+  SizedBox(
+    width: 20,
+  ),
+  Icon(Icons.arrow_circle_left_outlined),
+  SizedBox(
+    width: 20,
+  )
+],
+        ) ,
+body:
+const Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+  children: [
+
+    Text('HOSSAM',
+    style: TextStyle(fontSize: 25,
+    backgroundColor: Color.fromARGB(222, 222, 125, 11),
+    ),)
+  ],
+)
+
+
+
+      ),
+    );
+
+
+  }
+
+  }
