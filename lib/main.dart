b3a3e835b63b752adf7e3277110ceaf79ec408bc
Hossam <hossamfathi050@outlook.com>
2024-07-08 00:11:32 +0300

    test modifi debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -4,53 +4,49 @@ void main() {
   runApp(MyApp());
 }
 
-  class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.arrow_circle_left_outlined),
+      // debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.arrow_circle_left_outlined),
 
-          title:
-          const Text(
+            title:
+            const Text(
               'HOSSAM FATHI',
-          style: TextStyle(fontSize: 28,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,)
-            ,),
-          centerTitle: true,
-actions: [
+              style: TextStyle(fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,)
+              ,),
+            centerTitle: true,
+            actions: [
 
-  Icon(Icons.mobile_friendly),
-  SizedBox(
-    width: 20,
-  ),
-  Icon(Icons.arrow_circle_left_outlined),
-  SizedBox(
-    width: 20,
-  )
-],
-        ) ,
-body:
-const Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  crossAxisAlignment: CrossAxisAlignment.center,
-  children: [
-    Text('HOSSAM Fathi',
-    style: TextStyle(fontSize: 25,
-    backgroundColor: Color.fromARGB(222, 222, 125, 11),
-    ),)
-  ],
-)
-
-
+              Icon(Icons.mobile_friendly),
+              SizedBox(
+                width: 20,
+              ),
+              Icon(Icons.arrow_circle_left_outlined),
+              SizedBox(
+                width: 20,
+              )
+            ],
+          ),
+          body:
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('HOSSAM Fathi',
+                style: TextStyle(fontSize: 25,
+                  backgroundColor: Color.fromARGB(222, 222, 125, 11),
+                ),)
+            ],
+          )
 
       ),
     );
-
-
   }
 
-  }
+}
